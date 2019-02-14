d0df4cf49947d1ae4c1a04e1b4df93bfe5d40b97
Uche Onuekwusi <34797646+Uchechukwu1@users.noreply.github.com>
2019-02-14 20:59:51 +0000

    Update mad.js

--- mad.js
+++ mad.js
@@ -1,12 +1,36 @@
-const options = document.querySelector("p")
+const submitButton =document.getElementById("submit")
+const pinInput = document.getElementById("pin")
+const dogRadio = document.getElementById("dog").checked
+const catRadio = document.getElementById("cat").checked
+const otherRadio= document.getElementById("other").checked
+const otherInput = document.getElementById("otherinput")
+const div = document.getElementById("message")
 
-console.log(options)
-const input = document.createElement("input")
-input.type = "checkbox"
-options.appendChild(input)
+function validInput() {
 
-for(let i = 0 ; i < options.length; i++){
-options[i].style.color = "green"
+    alert("Your input was valid");
+  }
 
- 
-}
+
+  
+
+function myFunction() {
+    // Get the checkbox
+    var checkBox = document.querySelector(".checkbox");
+    // Get the output text
+    var email = document.querySelector(".email");
+  
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true){
+      email.style.display = "block";
+    } else {
+      email.style.display = "none";
+    }
+  }
+
+  
+  if(dogRadio === true ||catRadio===true){
+
+otherInput.value = ""
+
+  }
